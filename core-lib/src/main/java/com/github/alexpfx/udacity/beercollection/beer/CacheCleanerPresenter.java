5839f749a1ee17412ed676f5187bf6b1bfd427f0
Alexandre A <alexpfba@gmail.com>
2018-01-08 15:26:49 -0200

    cache cleaner classes

---
+++ core-lib/src/main/java/com/github/alexpfx/udacity/beercollection/beer/CacheCleanerPresenter.java
@@ -0,0 +1,8 @@
+package com.github.alexpfx.udacity.beercollection.beer;
+
+/**
+ * Created by alexandre on 08/01/18.
+ */
+
+public class CacheCleanerPresenter {
+}
---
+++ database-lib/src/main/java/com/github/alexpfx/udacity/beercollection/databaselib/BeerLocalDataSourceImpl.java
@@ -0,0 +1,8 @@
+package com.github.alexpfx.udacity.beercollection.databaselib;
+
+/**
+ * Created by alexandre on 08/01/18.
+ */
+
+class BeerLocalDataSource {
+}
