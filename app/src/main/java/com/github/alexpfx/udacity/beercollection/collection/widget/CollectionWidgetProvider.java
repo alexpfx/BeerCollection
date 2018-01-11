b4c5f99a31234d09c0b7b67d0e91ff0e4b1a7835
alexpfx <alexpfba@gmail.com>
2018-01-10 23:37:03 -0200

    widget

---
+++ app/src/main/java/com/github/alexpfx/udacity/beercollection/collection/widget/CollectionWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.github.alexpfx.udacity.beercollection.collection.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+public class CollectionWidgetProvider extends AppWidgetProvider {
+
+
+
+}
