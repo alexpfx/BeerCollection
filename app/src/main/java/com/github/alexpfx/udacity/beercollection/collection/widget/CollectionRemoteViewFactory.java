45b9d50933f526ecbc94d9c2453317dbaa03b87f
Alexandre A <alexpfba@gmail.com>
2018-01-11 20:56:55 -0200

    arquivo dimens.xml apagado por engano

---
+++ app/src/main/java/com/github/alexpfx/udacity/beercollection/collection/widget/CollectionRemoteViewFactory.java
@@ -0,0 +1,10 @@
+package com.github.alexpfx.udacity.beercollection.collection.widget;
+
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by alexandre on 11/01/18.
+ */
+
+class CollectionRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory {
+}
---
+++ app/src/main/java/com/github/alexpfx/udacity/beercollection/collection/widget/CollectionRemoteViewService.java
@@ -0,0 +1,8 @@
+package com.github.alexpfx.udacity.beercollection.collection.widget;
+
+/**
+ * Created by alexandre on 11/01/18.
+ */
+
+public class CollectionRemoteViewService {
+}
